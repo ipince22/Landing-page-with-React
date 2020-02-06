2c0491c2649c56e0f9f81ee5114217902ad23626
Daniel Ipince <ipince22@gmail.com>
2020-02-06 01:57:14 +0000

    using propTypes

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,24 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Card() {
+export function Card(props) {
 	return (
 		<div className="card" style={{ width: "300px" }}>
-			<img
-				className="card-img-top"
-				alt="..."
-				src="https://randomuser.me/api/portraits/women/24.jpg"
-			/>
+			<img className="card-img-top" alt="..." src={props.img} />
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</p>
-				<a href="#" className="btn btn-primary">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.body}</p>
+				<a href={props.link} className={"btn btn-" + props.buttoncolor}>
 					Go somewhere
 				</a>
 			</div>
 		</div>
 	);
 }
+
+Card.propTypes = {
+	img: PropTypes.string,
+	title: PropTypes.string,
+	body: PropTypes.string,
+	link: PropTypes.string,
+	buttoncolor: PropTypes.string
+};
+//src="https://randomuser.me/api/portraits/women/24.jpg"
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -15,10 +15,34 @@ export function Home() {
 				<Navbar />
 				<Jumbotron />
 				<div className="d-flex justify-content-center">
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+					<Card
+						img={"https://randomuser.me/api/portraits/women/24.jpg"}
+						body={"my firts card"}
+						title={"Laura Sosa"}
+						link={"http://www.google.com"}
+						buttoncolor={"primary"}
+					/>
+					<Card
+						img={"https://randomuser.me/api/portraits/women/25.jpg"}
+						body={"my second card"}
+						title={"Lesli Perona"}
+						link={"http://www.reactjs.org"}
+						buttoncolor={"info"}
+					/>
+					<Card
+						img={"https://randomuser.me/api/portraits/women/26.jpg"}
+						body={"my third card"}
+						title={"Martha Sanchez"}
+						link={"http://www.amazon.com"}
+						buttoncolor={"success"}
+					/>
+					<Card
+						img={"https://randomuser.me/api/portraits/women/27.jpg"}
+						body={"my fourth card"}
+						title={"Ana Laura Cepeda"}
+						link={"http://www.github.com"}
+						buttoncolor={"danger"}
+					/>
 				</div>
 			</div>
 			<Footer />
